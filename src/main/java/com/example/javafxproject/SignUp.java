6c46051e8c3b9d51aa9dc7be6aea54b2a2484e2b
Moniem <abdelmoniem.m.a.o@gmail.com>
2025-12-14 11:28:52 +0200

    delete toggle buttons

--- src/main/java/com/example/javafxproject/SignUp.java
+++ src/main/java/com/example/javafxproject/SignUp.java
@@ -12,19 +12,14 @@ public class SignUp {
     GridPane rootGridPane;
     Label nameLabel;
     TextField nameTextField;
-    Label emailLabel;
-    TextField emailTextField;
+    Label idLabel;
+    TextField idTextfield;
     Label passwordLabel;
     PasswordField passwordTextField;
     Label confirmPasswordLabel;
     PasswordField confirmPasswordField;
     Button signUpButton;
-    ToggleGroup userTypeToggleGroup;
-    RadioButton userRadioButton;
-    RadioButton adminRadioButton;
     Scene signUpScene;
-    HBox radioButtonsHBox;
-
     public SignUp() {
         initControls();
         renderControls();
@@ -34,22 +29,14 @@ public class SignUp {
         rootGridPane = new GridPane();
         nameLabel = new Label("Name:");
         nameTextField = new TextField();
-        emailLabel = new Label("Email:");
-        emailTextField = new TextField();
+        idLabel = new Label("Id:");
+        idTextfield = new TextField();
         passwordLabel = new Label("Password:");
         passwordTextField = new PasswordField();
         confirmPasswordLabel = new Label("Confirm Password:");
         confirmPasswordField = new PasswordField();
         signUpButton = new Button("Sign Up");
         signUpButton.setId("DownloadButton");
-        userTypeToggleGroup = new ToggleGroup();
-        userRadioButton = new RadioButton("user");
-        adminRadioButton = new RadioButton("admin");
-
-        userTypeToggleGroup.getToggles().addAll(userRadioButton, adminRadioButton);
-
-        radioButtonsHBox = new HBox(20, userRadioButton, adminRadioButton);
-        radioButtonsHBox.setAlignment(Pos.CENTER_LEFT);
 
         rootGridPane.setVgap(10);
         rootGridPane.setHgap(10);
@@ -59,14 +46,13 @@ public class SignUp {
     void renderControls() {
         rootGridPane.add(nameLabel, 0, 0);
         rootGridPane.add(nameTextField, 1, 0);
-        rootGridPane.add(emailLabel, 0, 1);
-        rootGridPane.add(emailTextField, 1, 1);
+        rootGridPane.add(idLabel, 0, 1);
+        rootGridPane.add(idTextfield, 1, 1);
         rootGridPane.add(passwordLabel, 0, 2);
         rootGridPane.add(passwordTextField, 1, 2);
         rootGridPane.add(confirmPasswordLabel, 0, 3);
         rootGridPane.add(confirmPasswordField, 1, 3);
-        rootGridPane.add(radioButtonsHBox, 1, 4);
-        rootGridPane.add(signUpButton, 1, 5, 2, 1);
+        rootGridPane.add(signUpButton, 1, 4, 2, 1);
     }
 
     public Scene getScene() {
